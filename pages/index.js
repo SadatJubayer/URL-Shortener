0c2235d8aad6a3f39f8e9de754aced3f0d3f4273
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 22:19:12 +0600

    👌 IMPROVE: URL submitting

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { Button, ErrorMessage, Input } from '../components/Atoms';
 import Layout from '../components/Layout';
 import { isValidUrl } from '../utils/checkUrl';
+import { shortUrl } from '../services/api';
+import Router from 'next/router';
+import { useURL } from '../services/context';
+
 const IndexPage = () => {
+  // const router = useRouter();
+  const { setUrl } = useURL();
   const [error, setError] = useState('');
   const [input, setInput] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const HandleSubmit = (e) => {
+  const HandleSubmit = async (e) => {
     e.preventDefault();
     setError('');
+    setLoading(true);
 
     // Validate URL
     if (!input || !isValidUrl(input)) {
+      setLoading(false);
       return setError('Please enter a valid URL.');
     }
 
     // Submit the URL
+    try {
+      const { data, status } = await shortUrl({
+        sourceUrl: input,
+      });
+      console.log(data);
+      if (status === 200) {
+        Router.push('/done');
+        setUrl({
+          mainUrl: input,
+          shortenedUrl: data.shortenedUrl,
+        });
+      }
+    } catch (error) {
+      console.log({ error });
+      setError('Something went wrong...');
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -29,7 +56,9 @@ const IndexPage = () => {
             autoFocus
             placeholder='Type/Paste your long URL'
           />
-          <Button type='submit'>Short it!</Button>
+          <Button type='submit' loading={loading}>
+            Short it!
+          </Button>
         </form>
         {error && <ErrorMessage text={error} />}
       </div>

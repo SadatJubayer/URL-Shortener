3a4708de2b069996eafb1af0c9951f1bfde1a405
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 15:18:37 +0600

    📦 NEW: Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9B51E0',
+      },
+    },
   },
   variants: {
     extend: {},

0e592699f9465f5ab32678692d4955fadcdab22f
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 17:24:36 +0600

    📦 NEW: URL Validator

---
+++ utils/checkUrl.js
@@ -0,0 +1,7 @@
+export const isValidUrl = (url) => {
+  const expression = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+
+  const regex = new RegExp(expression);
+
+  return regex.test(url);
+};

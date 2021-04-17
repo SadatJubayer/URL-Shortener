54a25fed1f16dca813a368824b126664f2d597b7
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 22:20:31 +0600

    👌 IMPROVE: Error messages

--- components/Atoms.jsx
+++ components/Atoms.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export const Button = (props) => {
-  const { children, ...rest } = props;
+  const { children, loading, ...rest } = props;
   return (
     <button {...rest} className='focus:outline-none bg-primary font-semibold text-base md:text-xl px-2 md:px-5 py-3'>
-      {children}
+      {loading ? 'Working...' : children}
     </button>
   );
 };
@@ -15,4 +15,13 @@ export const Input = (props) => {
 
 export const ErrorMessage = ({ text }) => {
   return <span className='text-red-400 self-start font-thin pl-2 md:pl-5'>{text}</span>;
+};
+
+export const CopyButton = (props) => {
+  const { children, ...rest } = props;
+  return (
+    <button {...rest} className='focus:outline-none bg-primary text-base md:text-xl px-2 py-1'>
+      {children}
+    </button>
+  );
 };

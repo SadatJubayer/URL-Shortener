647c9842bb59b90f1f3d30f0b141c9a11727d210
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 15:54:22 +0600

    👌 IMPROVE: Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+  const router = useRouter();
   return (
     <header className='py-5'>
       <nav className='flex items-center'>
-        <img className='h-12 md:h-16 w-12 md:w-16 bg-white rounded-full p-1.5 mr-2.5' src='/logo.svg' alt='' />
+        <img
+          onClick={() => router.push('/')}
+          className='h-12 md:h-16 w-12 md:w-16 bg-purple-50 rounded-full p-1.5 mr-2.5 cursor-pointer hover:bg-purple-100'
+          src='/logo.svg'
+          alt=''
+        />
         <div>
           <h1 className='text-xl md:text-3xl font-bold'>GET in SHORT</h1>
           <p className='mt-1 text-base md:text-lg font-thin'>

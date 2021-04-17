789abab3d4c87c4a9eb2a2fceab4aa139ab1fe21
Sadat Jubayer <sadatjubayer@gmail.com>
2021-04-17 15:54:10 +0600

    👌 IMPROVE: Styled Error Message

--- components/Atoms.jsx
+++ components/Atoms.jsx
@@ -13,3 +13,6 @@ export const Input = (props) => {
   return <input {...props} className='focus:outline-none bg-gray-800 text-base md:text-xl px-2 md:px-5 py-3 flex-1' />;
 };
 
+export const ErrorMessage = ({ text }) => {
+  return <span className='text-red-500 self-start font-thin pl-2 md:pl-5'>{text}</span>;
+};
